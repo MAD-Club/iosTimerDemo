37dd8cb5786c91be7c415841113f8cda20e62a59
cynthia <cyn.zhou@mail.utoronto.ca>
2024-09-10 00:44:28 -0400

    Initial Commit

---
+++ iOSDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSDemo
+//
+//  Created by Cynthia Zhou on 2024-09-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSDemo/iOSDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSDemoApp.swift
+//  iOSDemo
+//
+//  Created by Cynthia Zhou on 2024-09-10.
+//
+
+import SwiftUI
+
+@main
+struct iOSDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
